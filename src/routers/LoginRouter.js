8846491bfb9b08f4170b60766db73ff987715ba4
hongwei <iamhongwei0417@gmail.com>
2018-07-23 17:24:02 +0800

    modify router.js and App.js

---
+++ src/routers/LoginRouter.js
@@ -0,0 +1,7 @@
+import { createStackNavigator } from 'react-navigation'
+import {} from '../login/LoginPage'
+
+
+export default createStackNavigator({
+
+})
--- src/routers/Router.js
+++ src/routers/Router.js
@@ -1,1 +1,14 @@
-import { createStackNavigator, createTabNavigator, createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator, createTabNavigator, createSwitchNavigator, createDrawerNavigator } from 'react-navigation'
+import AuthRouter from './LoginRouter'
+
+
+const AppRouter = createTabNavigator({
+    //五個主要功能TAB
+})
+
+
+export default createSwitchNavigator({
+    App: AppRouter,
+    Auth: AuthRouter
+})
+
