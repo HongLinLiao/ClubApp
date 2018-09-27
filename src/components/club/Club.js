f1de89bd409e7f12658ef0c6a8b28caa7de25afa
HongLinLiao <leocin200907@gmail.com>
2018-09-27 16:59:43 +0800

    修正

--- src/components/club/Club.js
+++ src/components/club/Club.js
@@ -13,7 +13,6 @@ import {
 import Expo from 'expo'
 
 import ModalDropdown from 'react-native-modal-dropdown';
-import PostComponent from '../post/PostData'
 import { randomClub } from '../../modules/Club'
 
 class Club extends React.Component {
