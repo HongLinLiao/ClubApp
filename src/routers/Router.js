736dc7eecfe62df31bb706b988cccef5b5f2b377
yuchilien <hh77017@gmail.com>
2018-10-04 17:15:59 +0800

    更新routers

--- src/routers/Router.js
+++ src/routers/Router.js
@@ -7,23 +7,69 @@ import FirstLoginRouter from './FirstLoginRouter'
 import UserRouter from './UserRouter'
 import SearchRouter from './SearchRouter'
 
+import Icon from 'react-native-vector-icons/Ionicons'
+import React from 'react'
 //第三層
 const MainRouter = createBottomTabNavigator({
-    HomeRouter: HomeRouter,
-    SearchRouter: SearchRouter,
-    ClubRouter: ClubRouter,
-    PersonalRouter: PersonalRouter,
-},
-{
-    navigationOptions: ({navigation}) => {
-        const { index, routes } = navigation.state
-        return {
-            tabBarVisible: !(routes[index].routeName == 'AddPost'),
-            animationEnabled: true,
-            swipeEnabled: true
+    Home: {
+        screen: HomeRouter,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+                <Icon name="ios-home" color={tintColor} size={24} />
+            )
         }
-    }
-})
+    },
+    Search: {
+        screen: SearchRouter,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+                <Icon name="ios-search" color={tintColor} size={24} />
+            )
+        }
+    },
+    Club: {
+        screen: ClubRouter,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+                <Icon name="ios-star" color={tintColor} size={24} />
+            )
+        }
+    },
+    Personal: {
+        screen: PersonalRouter,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+                <Icon name="ios-settings" color={tintColor} size={24} />
+            )
+        }
+    },
+},
+
+    {
+        initialRouteName: 'Home',
+        order: ['Search', 'Home', 'Club', 'Personal'],
+        swipeEnabled: true,//滑動可換頁
+        tabBarOptions: {
+            activeTintColor: '#f6b456',//字有按
+            inactiveTintColor: '#0d4273',//字沒按
+            activeBackgroundColor: '#0d4273',//背景有按
+            inactiveBackgroundColor: '#f6b456',//背景沒按
+
+        }
+
+    },
+
+    {
+        navigationOptions: ({ navigation }) => {
+            const { index, routes } = navigation.state
+            return {
+                tabBarVisible: !(routes[index].routeName == 'AddPost'),
+                animationEnabled: true,
+                swipeEnabled: true
+            }
+
+        }
+    })
 
 //第二層
 const AppRouter = createSwitchNavigator({
--- src/routers/SearchRouter.js
+++ src/routers/SearchRouter.js
@@ -1,9 +1,58 @@
 import { createStackNavigator } from 'react-navigation'
 import SearchPage from '../containers/search/SearchPage'
-
+import { TextInput, StyleSheet, Image } from 'react-native'
+import React from 'react'
+import { View } from 'native-base';
 
 export default createStackNavigator({
     Search: {
-        screen: SearchPage
+        screen: SearchPage,
+        navigationOptions: ({ navigation }) => ({
+            headerTitleStyle: {
+                color: '#666666',
+                fontSize: 20,
+            },
+            headerStyle: {
+                backgroundColor: 'white',
+            },
+            headerTitle:
+
+                <View style={styles.searchView}>
+                    <TextInput
+                        placeholder="搜尋 (ex:長庚大學)"
+                        placeholderTextColor='gray'
+                        style={{
+                            height: 30,
+                            padding: 10,
+                            fontSize: 18,
+                            flex: 1,
+                        }} />
+                    <Image source={require('../images/images2/search.png')}
+                        style={styles.search} />
+                </View>
+            ,
+        })
     }
+})
+
+const styles = StyleSheet.create({
+    search: {
+        height: 18,
+        width: 18,
+        marginRight: 10
+    },
+    searchView: {
+        flex: 1,
+        height: 35,
+        flexDirection: 'row',
+        borderWidth: 1,
+        borderColor: 'rgba(102,102,102,0.2)',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 10,
+    },
+
 })
