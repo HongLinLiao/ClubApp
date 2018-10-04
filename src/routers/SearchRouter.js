1cfa4581771c8146c2bd8a5e8359151242c2680a
liwen <s981322ccp@gmail.com>
2018-10-04 15:16:44 +0800

    persearrouter

--- src/routers/SearchRouter.js
+++ src/routers/SearchRouter.js
@@ -1,7 +1,8 @@
 import { createStackNavigator } from 'react-navigation'
 import SearchPage from '../containers/search/SearchPage'
-import { TextInput } from 'react-native'
+import { TextInput, StyleSheet, Image } from 'react-native'
 import React from 'react'
+import { View } from 'native-base';
 
 export default createStackNavigator({
     Search: {
@@ -12,12 +13,47 @@ export default createStackNavigator({
                 fontSize: 20,
             },
             headerStyle: {
-                backgroundColor: '#f6b456'
+                backgroundColor: 'white'
             },
-            headerLeft: (
-                <TextInput>
-                </TextInput>
-            )
+            headerTitle:
+
+                <View style={styles.searchView}>
+                    <TextInput
+                        placeholder="搜尋（ex:長庚大學）"
+                        placeholderTextColor='gray'
+                        style={{
+                            height: 30,
+                            padding:10,
+                            backgroundColor: 'white',
+                            flex: 1,
+                            fontSize:15,
+                        }} />
+                    <Image source={require('../images/images2/search.png')}
+                        style={styles.search} />
+                </View>
+            ,
         })
     }
+})
+
+const styles = StyleSheet.create({
+    search: {
+        height: 18,
+        width: 18,
+        marginRight: 10
+    },
+    searchView: {
+        height: 33,
+        flexDirection: 'row',
+        borderWidth: 1,
+        borderColor: 'rgba(102,102,102,0.2)',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 13,
+        flex: 1,
+    },
+
 })
