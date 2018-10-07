750fe710571b9aba54fcd658be6a3925f5aeeafd
b0444124 <36983065+b0444124@users.noreply.github.com>
2018-10-07 22:22:09 +0800

    創社團+社團隱私

--- src/styles/personal/CreateClub.js
+++ src/styles/personal/CreateClub.js
@@ -10,30 +10,31 @@ export default StyleSheet.create({
     },
     title: {
         color: 'rgba(102,102,102,1)',
-        fontSize: 22,
+        fontSize: 20,
         alignSelf: 'center',
         margin: 40
     },
     Q: {
         color: 'rgba(102,102,102,1)',
-        fontSize: 22,
+        fontSize: 20,
         alignSelf: 'center',
         margin:5
     },
     textInput: {
         marginBottom: 25,
-        width: 210,
+        width: 200,
         height: 40,
         color: 'rgba(102,102,102,1)',
-        backgroundColor: 'rgba(246,180,86,0.4)',
+        backgroundColor: 'rgba(246,180,86,0.3)',
         borderRadius: 10,
         textAlign: 'center',
         fontSize:15
     },
     nextText: {
         color: 'rgba(102,102,102,0.5)',
-        fontSize: 18,
+        fontSize: 15,
         alignSelf: 'center',
-        marginTop: 20
+        marginTop: 30
+
     }
 })
