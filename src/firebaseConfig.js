ab2cf62c88a185921dfb7dc51d1bf90c5fed16d8
HongLinLiao <honglin0822@outlook.com>
2019-03-06 15:44:02 +0800

    zip

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyCoXJhUC3rgX0bZCM6JzuZoVewqFF6qRjs",
-  authDomain: "clubapp-f9473.firebaseapp.com",
-  databaseURL: "https://clubapp-f9473.firebaseio.com",
-  projectId: "clubapp-f9473",
-  storageBucket: "clubapp-f9473.appspot.com",
-  messagingSenderId: "951052400924"
-}
